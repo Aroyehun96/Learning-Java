362ff19253182847d0838c342682b33eedbba46c
@Aroyehun96 <aroyehunhabeeb@gmail.com>
2020-03-30 10:56:36 +0100

    firstcommit

---
+++ Myfirstjavaprog.java
@@ -0,0 +1,5 @@
+ class Myfirstjavaprog{  
+    public static void main(String args[]){  
+     System.out.println("Aroyehun Habeeb Morenikeji");  
+    }  
+}  
--- myfirstprogram.java
+++
@@ -1,5 +0,0 @@
-class Myfirstjavaprogram{
-    public static void main(string args{}){
-        system.out.println("Aroyehun Habeeb Morenikeji Akanni");
-    }
-}
